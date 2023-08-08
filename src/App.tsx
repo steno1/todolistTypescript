bbb9f9edebcbdfb183d790a421835964b45ad636
steno1 <onuprinceley@gmail.com>
2023-08-08 10:24:31 +0100

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,60 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+// Import the Bootstrap CSS for styling
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import {Navigate, Route, Routes} from "react-router-dom"
+// Import necessary components and functions from react-router-dom
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import { Container } from "react-bootstrap"
-import NewNotes from "./NewNotes"
+// Import a specific component from the react-bootstrap library
+import { Container } from "react-bootstrap";
+// Import the NewNotes component from "./NewNotes" file
+import NewNotes from "./NewNotes";
 
+// Define a type "Note" which combines "id" and "NoteData" properties
+export type Note = {
+  id: string;
+} & NoteData;
+
+// Define a type "NoteData" to represent properties of a note
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+// Define a type "Tag" with "id" and "label" properties
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+// The main function/component of your application
 function App() {
-  
   return (
+    // Use the Container component from react-bootstrap to structure your content
     <Container className="my-4">
+      {/* Define routes for navigation using react-router-dom */}
+      <Routes>
+        {/* Route for the homepage */}
+        <Route path="/" element={<h1>Hi steno friend</h1>} />
 
-    
-  <Routes>
-    <Route path='/' element={<h1>Hi steno friend</h1>}/>
-    <Route path='/new' element={<NewNotes/>}/>
-    <Route path='*' element={<Navigate to='/'/>}/>
-    <Route path="/:id">
-      <Route index element={<h1>Show</h1>}/>
-      <Route path='edit' element={<h1>Edit</h1>}/>
+        {/* Route for the "new" page */}
+        <Route path="/new" element={<NewNotes />} />
 
-    </Route>
-  </Routes>
-  </Container>
-   )
-    
+        {/* Wildcard route, redirects to the homepage for invalid URLs */}
+        <Route path="*" element={<Navigate to="/" />} />
+
+        {/* Nested route with a parameter ":id" */}
+        <Route path="/:id">
+          {/* Sub-route for the index page of a specific note */}
+          <Route index element={<h1>Show</h1>} />
+          
+          {/* Sub-route for the "edit" page of a specific note */}
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+// Export the main App component as the default export
+export default App;
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,26 +1,107 @@
-import { Col, Form, Row, Stack } from "react-bootstrap";
+/* Import necessary components and functions from react-bootstrap*/
 
-const NoteForm = () => {
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import React, { FormEvent, useRef, useState } from "react";
+
+import CreatableReactSelect from "react-select/creatable";
+import { Link } from "react-router-dom";
+import { NoteData } from "./App";
+import { Tag } from "./App";
+
+/* Import React module, FormEvent type, useRef, and useState from react */
+
+/* Import CreatableReactSelect from the react-select library */
+
+/* Import Link component from react-router-dom */
+
+// Import Tag type from "./App"
+
+
+
+// Define type for props that NoteForm component receives
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+// Define the NoteForm component
+const NoteForm = ({ onSubmit }: NoteFormProps) => {
+  // Create a reference for the title input element
+  const titleRef = useRef<HTMLInputElement>(null);
+  // Create a reference for the markdown textarea element
+  const markDownRef = useRef<HTMLTextAreaElement>(null);
+  // Use state to manage selected tags
+  const [selectedTags, setSelectedTags] = useState<Tag[] | undefined>();
+
+  // Define a function to handle form submission
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    
+    // Call the onSubmit function with the form data
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markDownRef.current!.value,
+      tags: selectedTags || [], // Use selectedTags or an empty array if undefined
+    });
+  }
+
+  // Return the JSX content of the NoteForm
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
-       <Row>
-<Col>
-<Form.Group controlId="title">
-    <Form.Label>Title</Form.Label>
-    <Form.Control required/>
-</Form.Group>
-</Col>
-<Col>
-<Form.Group controlId="tags">
-    <Form.Label>Tags</Form.Label>
-    <Form.Control required/>
-</Form.Group>
-</Col>
-       </Row>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              {/* Attach the titleRef to the title input */}
+              <Form.Control required ref={titleRef} />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              {/* Use CreatableReactSelect for tag selection */}
+              <CreatableReactSelect
+                value={
+                  selectedTags?.map((tag) => ({
+                    label: tag.label,
+                    value: tag.id,
+                  })) || []
+                }
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => ({
+                      label: tag.label,
+                      id: tag.value,
+                    }))
+                  );
+                }}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          {/* Attach the markDownRef to the markdown textarea */}
+          <Form.Control required as="textarea" rows={10} ref={markDownRef} />
+        </Form.Group>
+        {/* Stack for buttons */}
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          {/* Save button */}
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          {/* Cancel button using Link */}
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
       </Stack>
     </Form>
   );
-}
+};
 
+// Export the NoteForm component as the default export
 export default NoteForm;
